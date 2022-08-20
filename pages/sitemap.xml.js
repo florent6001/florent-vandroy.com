75dbeef5a82d59859f3e8061c1bda3b6cda551ff
Florent Vandroy <florentvandroy@gmail.com>
2022-08-20 19:57:00 +0200

    add spaces

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -4,32 +4,32 @@ const base_url = 'https://florent-vandroy.fr/'
 
 function generateSiteMap(posts) {
   return `<?xml version="1.0" encoding="UTF-8"?>
-   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-        <loc>${`${base_url}`}</loc>
-    </url>
-    <url>
-        <loc>${`${base_url}mentions-legales`}</loc>
-    </url>
-    <url>
-        <loc>${`${base_url}politique-de-confidentialite-des-donnees`}</loc>
-    </url>
-    <url>
-        <loc>${`${base_url}realisations`}</loc>
-    </url>
-    <url>
-        <loc>${`${base_url}blog`}</loc>
-    </url>
-     ${posts
-       .map((post) => {
-         return `
-       <url>
-           <loc>${`${base_url}blog/${post}`}</loc>
-       </url>
-     `;
-       })
-       .join('')}
-   </urlset>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <url>
+            <loc>${`${base_url}`}</loc>
+        </url>
+        <url>
+            <loc>${`${base_url}mentions-legales`}</loc>
+        </url>
+        <url>
+            <loc>${`${base_url}politique-de-confidentialite-des-donnees`}</loc>
+        </url>
+        <url>
+            <loc>${`${base_url}realisations`}</loc>
+        </url>
+        <url>
+            <loc>${`${base_url}blog`}</loc>
+        </url>
+        ${posts
+        .map((post) => {
+            return `
+        <url>
+            <loc>${`${base_url}blog/${post}`}</loc>
+        </url>
+        `;
+        })
+        .join('')}
+    </urlset>
  `;
 }
 
