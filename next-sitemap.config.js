f1b657afe3ae0f14e66491ef8f14f5506dff0043
Florent Vandroy <florentvandroy@gmail.com>
2022-08-21 12:36:57 +0200

    Add next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.florent-vandroy.fr',
+    generateRobotsTxt: true, // (optional)
+    generateIndexSitemap: false
+  }
